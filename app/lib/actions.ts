5e7a4b9e4d191ac2bc46b88fc16a4c5e4c1f4c3c
Moskalenko Serhii <102375066+viamur@users.noreply.github.com>
2024-01-02 15:21:55 +0200

    add error for edit-form invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -38,9 +38,6 @@ export async function createInvoice(prevState: State, formData: FormData) {
         status: formData.get('status'),
     });
 
-    console.log(validatedFields)
-    console.log(formData)
-
     if (!validatedFields.success) {
         return {
             errors: validatedFields.error.flatten().fieldErrors,
@@ -67,13 +64,25 @@ export async function createInvoice(prevState: State, formData: FormData) {
     redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+    id: string,
+    prevState: State,
+    formData: FormData,
+) {
+    const validatedFields = UpdateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
 
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Update Invoice.',
+        };
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
 
     try {
