3e82407f4ba8278576bf46bb3dada5c3a7a14c6c
Moskalenko Serhii <102375066+viamur@users.noreply.github.com>
2024-01-04 20:25:42 +0200

    add customers table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,28 @@
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/invoices/table';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { lusitana } from '@/app/ui/fonts';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import {fetchCustomers, fetchFilteredCustomers, fetchInvoicesPages} from '@/app/lib/data';
 import { Metadata } from 'next';
+import CustomersTable from '@/app/ui/customers/table';
 
 export const metadata: Metadata = {
     title: 'Customers'
 };
 
-export default function Page() {
-    return <p>Customers Page</p>;
+
+export default async function Page({
+    searchParams,
+     }: {
+    searchParams?: {
+        query?: string;
+    };
+}) {
+    const customers = await fetchFilteredCustomers(searchParams?.query || '');
+    return (
+        <CustomersTable customers={customers} />
+    );
 }
